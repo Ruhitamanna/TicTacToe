62232c840851ec8244ad74869d27a6e1d1e6e226
cis101 <cis101@secs.iub.edu.bd>
2017-06-04 10:51:21 +0600

    Assignment 6

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,14 @@
 //JS code goes here
+/*var cell1=window.document.getElementBYID('cell1');
+console.log(window.document.getElementBYID'cell1');
+function cellClicked(event){
+	event.target.innerHTML="X";
+}*/
+//cell1.addEven
+
+var turn;
+function cellclicked(event){
+event.target.innerHTML=turn?"0" : "X";
+turn!=turn;
+
+}
